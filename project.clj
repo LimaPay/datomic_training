fe4f490c347320e708f650ca08f05fa0583dd3c3
Damien Stanton <email@damienkstanton.com>
2015-06-08 16:29:39 -0400

    added clojurescript and 1.7RC1

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Sample Code for Day of Datomic Presentation"
   :plugins [[lein-tg "0.0.1"]]
   :jvm-opts ["-Xmx1g" "-server"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/clojurescript "0.0-3308"]
                  [org.clojure/test.generative "0.3.0"]
                  [com.datomic/datomic-free "0.9.5153"]
                  [incanter/incanter-charts "1.3.0"]
